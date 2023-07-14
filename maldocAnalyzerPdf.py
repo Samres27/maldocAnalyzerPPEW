6568a207125146a56745a960ed0ae7d9085a9d0c
samres27 <yosoyam4@gmail.com>
2023-07-14 18:54:08 -0400

    added a version to drag and drop

--- maldocAnalyzerPdf.py
+++ maldocAnalyzerPdf.py
@@ -3,38 +3,39 @@ import os
 class maldocAnalyzerPdf:
     def __init__(self):
         self.readCVS()
-        self.inserFileSearch()
-        
-        
-        self.cleanAuxFile()
-    def inserFileSearch(self):
-        arch=input("insert File:     ")
+    def inserFileSearch(self,arch):
+        textoutput=""
         listFile=self.extractData(arch)
-        vHeader=listFile[5]
-        Vobj=listFile[7]
-        Vendobj=listFile[9]
-        Vstream=listFile[11]
-        Vendstream=listFile[13]
-        Vxref=listFile[15]
-        Vtrailer=listFile[17]
-        Vstartxref=listFile[19]
-        Vpage=listFile[21]
-        Vencrypt=listFile[23]
-        VobjStm=listFile[25]
-        vJS=listFile[27]
-        VJavaScript=listFile[29]
-        vAA=listFile[31]
-        VopenAction=listFile[33]
-        VAcroForm=listFile[35]
-        VJBIG2Decode=listFile[37]
-        VRichMedia=listFile[39]
-        Vlaunch=listFile[41]
-        VEmbeddedFile =listFile[43]
-        vXFA=listFile[45]
-        vURI=listFile[47]
-        vColor=listFile[51]
-        self.searchMalware(vHeader,Vobj,Vendobj,float(Vstream),Vendstream,Vxref,float(Vtrailer),Vstartxref,Vpage,float(Vencrypt),float(VobjStm),vJS,VJavaScript,vAA,VopenAction,VAcroForm,VJBIG2Decode,VRichMedia,Vlaunch,VEmbeddedFile,vXFA,float(vColor))
+        if(len(listFile)>50):
+            vHeader=listFile[5]
+            Vobj=listFile[7]
+            Vendobj=listFile[9]
+            Vstream=listFile[11]
+            Vendstream=listFile[13]
+            Vxref=listFile[15]
+            Vtrailer=listFile[17]
+            Vstartxref=listFile[19]
+            Vpage=listFile[21]
+            Vencrypt=listFile[23]
+            VobjStm=listFile[25]
+            vJS=listFile[27]
+            VJavaScript=listFile[29]
+            vAA=listFile[31]
+            VopenAction=listFile[33]
+            VAcroForm=listFile[35]
+            VJBIG2Decode=listFile[37]
+            VRichMedia=listFile[39]
+            Vlaunch=listFile[41]
+            VEmbeddedFile =listFile[43]
+            vXFA=listFile[45]
+            vURI=listFile[47]
+            vColor=listFile[51]
+            textoutput+=self.searchMalware(vHeader,Vobj,Vendobj,float(Vstream),Vendstream,Vxref,float(Vtrailer),Vstartxref,Vpage,float(Vencrypt),float(VobjStm),vJS,VJavaScript,vAA,VopenAction,VAcroForm,VJBIG2Decode,VRichMedia,Vlaunch,VEmbeddedFile,vXFA,float(vColor))
+        else:
+            textoutput+="document not found or format it not pdf"+"\n"
+        return textoutput
     def extractData(self,filename):
+        
         text="python3 .//necessaryPrograms//pdfid//pdfid.py "+filename+"  --output=pdfInfo.txt >end "
         os.system(text)
 
@@ -52,6 +53,7 @@ class maldocAnalyzerPdf:
     def searchMalware(self,header,obj,endobj,stream,endstream,xref,trailer,startxref,pageno,encrypt,ObjStm,JS,Javascript,
                       AA,OpenAction,Acroform,JBIG2Decode,RichMedia,launch,EmbeddedFile,XFA,Colors):
         #in this part it is not necessary like this, but it is more understandable this way
+        textOutput=""
         value=self.df[self.df["header"]=="\t"+header]
         value=value[value["obj"]==obj]
         value=value[value["endobj"]==endobj]
@@ -80,31 +82,38 @@ class maldocAnalyzerPdf:
         value=value[value["Colors"]==Colors]
         
         if(value['Class'].empty):
-            print("there are no matches the pdf file is in an insecure state")
-            print("reviewing structure to determine risk")
-            self.analizerPdf(OpenAction,AA,Javascript,JS,launch,RichMedia,ObjStm,JBIG2Decode,)
+            textOutput+="there are no matches the pdf file is in an insecure state"+"\n"
+            textOutput+="reviewing structure to determine risk"+"\n"
+            textOutput+=self.analizerPdf(OpenAction,AA,Javascript,JS,launch,RichMedia,ObjStm,JBIG2Decode,)
         else:
-            print("the pdf documents is:  ",value["Class"].values[0])
+            textOutput+="the pdf documents is:  "+value["Class"].values[0]+"\n"
+        return textOutput
     def analizerPdf(self,OpenAction,AA,Javascript,JS,launch,RichMedia,ObjStm,JBIG2Decode):
+        textOutput=""
         OpenAction=float(OpenAction)
         AA=float(AA);JS=float(JS)
         Javascript=float(Javascript); launch=float(launch); RichMedia=float(RichMedia); JBIG2Decode=float(JBIG2Decode)
         vl=(OpenAction>0 or AA>0 or JBIG2Decode>0) 
         if(vl):
-            print(  "the document is very suspicious, possibly it is maldoc")
+            textOutput+="the document is very suspicious, possibly it is maldoc"+"\n"
         elif(RichMedia>0 or ObjStm>0):
-            print(" nothing directly malicious was found, but the document could be hiding its intentions in:",end="")
-            print("RichMedia=  ",RichMedia,"objStm=   ",ObjStm)
+            textOutput+=" nothing directly malicious was found, but the document could be hiding its intentions in:"
+            textOutput+="RichMedia=  "+str(RichMedia)+"  objStm=   "+str(ObjStm)+"\n"
         elif((Javascript>0 or JS>0 or launch>0 )):
-            print(" the document is running some kind of javascript or JS, so we advise you to proceed carefully if it asks you to activate or open something")
+            textOutput+=" the document is running some kind of javascript or JS, so we advise you to proceed carefully if it asks you to activate or open something"+"\n"
         else:
-            print(" nothing suspicious in this document")
+            textOutput+=" nothing suspicious in this document\n"
+        return textOutput
     def cleanAuxFile(self):
         archive=open("pdfInfo.txt",'w')
         archive.write('')        
         
         
         
-hl=maldocAnalyzerPdf()
+if  __name__=='__main__':
+    pr=maldocAnalyzerPdf()
+    arch=input("insert File:     ")
+    print(pr.inserFileSearch(arch))
+    pr.cleanAuxFile()
    
 
---
+++ maldocAnalyzerPdfVersionDND.py
@@ -0,0 +1,53 @@
+from maldocAnalyzerPdf import maldocAnalyzerPdf
+from tkinter import *
+from tkinterdnd2 import *
+
+class versionDND:
+    def __init__(self):
+        self.analyser=maldocAnalyzerPdf()
+        self.analyser.readCVS()
+        self.root = TkinterDnD.Tk()
+        self.root.geometry("400x400")
+        self.entry_sv = StringVar()
+        self.entry_sv.set("drag and drop the PDF")
+        self.images=PhotoImage(file="PDFIco.png")
+        self.MgsText=StringVar()
+        self.MgsText.set("")
+        self.entry = Label(self.root,textvariable=self.entry_sv,width=200,fg="black")
+        self.entry2=Label(self.root,textvariable=self.MgsText,width=300)
+        self.entry2.place(x=30,y=200,width=350,height=200)
+    #entry.pack(padx=10,pady=10)
+        self.entry.place(x=75,y=50,width=250,height=200)
+
+        self.entry.drop_target_register(DND_FILES)
+        self.entry.dnd_bind('<<Drop>>', self.drop)
+        
+
+        self.root.mainloop()
+    def drop(self,event):
+        self.extractDoc(event.data)
+    def extractDoc(self,vl):
+        vl=str(vl)
+        textParse=vl.split("/")
+        if(".pdf" in textParse[-1]):
+            self.entry=Label(self.root,image=self.images,width=200)
+            self.entry.place(x=75,y=50,width=250,height=200)
+            self.entry.bind("<Button-1>",self.restart)
+            self.MetMaldocAnalyzerPdf(vl)
+        else:
+            self.entry_sv.set("sorry the document is not pdf")
+    def MetMaldocAnalyzerPdf(self,direction):
+        
+        self.MgsText.set(self.analyser.inserFileSearch(direction) +"\n if you have another pdf that you want to analyze, click the icon")
+        self.analyser.cleanAuxFile()  
+       
+    def restart(self,event):
+        self.entry_sv.set("drag and drop the PDF")
+        self.entry = Label(self.root,textvariable=self.entry_sv,width=200,fg="black")
+
+        self.entry.place(x=75,y=50,width=250,height=200)
+        self.entry.drop_target_register(DND_FILES)
+        self.entry.dnd_bind('<<Drop>>', self.drop)
+        self.MgsText.set(" ")
+       
+versionDND()
